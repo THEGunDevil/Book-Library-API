46eb680762682781a8f26c138f4809507ee32af5
Learning React <himelsd117@gmail.com>
2025-10-05 01:53:59 +0600

    --allow-empty-massage

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"fmt"
 	"os"
 )
 
@@ -14,12 +15,19 @@ type Config struct {
 }
 
 func LoadConfig() Config {
-	return Config{
-		DBHost:     os.Getenv("DB_HOST"),
-		DBPort:     os.Getenv("DB_PORT"),
-		DBUser:     os.Getenv("DB_USER"),
-		DBPassword: os.Getenv("DB_PASSWORD"),
-		DBName:     os.Getenv("DB_NAME"),
-		DB_URL:     os.Getenv("DATABASE_URL"),
-	}
+    return Config{
+        DBHost:     os.Getenv("DB_HOST"),
+        DBPort:     os.Getenv("DB_PORT"),
+        DBUser:     os.Getenv("DB_USER"),
+        DBPassword: os.Getenv("DB_PASSWORD"),
+        DBName:     os.Getenv("DB_NAME"),
+        DB_URL:     fmt.Sprintf(
+            "postgres://%s:%s@%s:%s/%s",
+            os.Getenv("DB_USER"),
+            os.Getenv("DB_PASSWORD"),
+            os.Getenv("DB_HOST"),
+            os.Getenv("DB_PORT"),
+            os.Getenv("DB_NAME"),
+        ),
+    }
 }
