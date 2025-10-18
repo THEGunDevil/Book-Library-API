85031f19da4eeda6a82369d7f40ddd3445d2f154
Learning React <himelsd117@gmail.com>
2025-10-18 15:01:57 +0600

    --allow-empty-message

--- internal/middleware/auth_middleware.go
+++ internal/middleware/auth_middleware.go
@@ -6,6 +6,7 @@ import (
 	"github.com/THEGunDevil/GoForBackend/internal/service"
 	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v4"
+	"github.com/google/uuid"
 )
 
 func AuthMiddleware() gin.HandlerFunc {
@@ -24,13 +25,32 @@ func AuthMiddleware() gin.HandlerFunc {
             return
         }
 
-        claims := token.Claims.(jwt.MapClaims)
-        c.Set("userID", claims["sub"])
+        claims, ok := token.Claims.(jwt.MapClaims)
+        if !ok {
+            c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "invalid token claims"})
+            return
+        }
+
+        subStr, ok := claims["sub"].(string)
+        if !ok {
+            c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "invalid token sub"})
+            return
+        }
+
+        userUUID, err := uuid.Parse(subStr)
+        if err != nil {
+            c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "invalid userID"})
+            return
+        }
+
+        // Set values in context
+        c.Set("userID", userUUID)
         c.Set("role", claims["role"])
         c.Set("token_version", claims["token_version"])
         c.Next()
     }
 }
+
 
 
 func AdminOnly() gin.HandlerFunc {
