6a3352207eb49e26b6ce8ff07c3e6b76386b7cc8
Learning React <himelsd117@gmail.com>
2025-10-20 15:19:46 +0600

    --allow-empty-message

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -52,6 +52,8 @@ func main() {
 		userGroup.GET("/", middleware.AdminOnly(), handlers.GetAllUsersHandler)
 		userGroup.GET("/user", handlers.GetUserHandler)
 		userGroup.GET("/user/:id", handlers.GetUserByIDHandler)
+		userGroup.GET("/profile/:id", handlers.GetProfileData)
+
 		// only admin can update user info
 		userGroup.PATCH("/:id", handlers.UpdateUserByIDHandler)
 	}
