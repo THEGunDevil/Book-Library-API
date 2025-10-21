22d500817113c68dd18ed47062731ab7a1d50fb6
Learning React <himelsd117@gmail.com>
2025-10-22 01:59:57 +0600

    --allow-empty-message

--- internal/db/migrations/0006_add_bio_in_users.sql
+++ internal/db/migrations/0006_add_bio_in_users.sql
@@ -2,8 +2,21 @@
 -- Add bio column
 ALTER TABLE users
 ADD COLUMN IF NOT EXISTS bio TEXT NOT NULL DEFAULT '';
+-- Add description column
+ALTER TABLE books
+ADD COLUMN IF NOT EXISTS description TEXT NOT NULL DEFAULT '';
+-- Add genre column
+ALTER TABLE books
+ADD COLUMN IF NOT EXISTS genre TEXT NOT NULL DEFAULT '';
 
 -- +goose Down
 -- Drop bio column
 ALTER TABLE users
 DROP COLUMN IF EXISTS bio;
+-- Drop description column
+ALTER TABLE books
+DROP COLUMN IF EXISTS description;
+
+-- Drop genre column
+ALTER TABLE books
+DROP COLUMN IF EXISTS genre;
