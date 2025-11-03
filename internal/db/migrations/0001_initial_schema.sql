678b9ea5ae53d25111c60e6cad1699c515a88547
Learning React <himelsd117@gmail.com>
2025-11-03 20:30:56 +0600

    --allow-empty-message

--- internal/db/migrations/0001_initial_schema.sql
+++ internal/db/migrations/0001_initial_schema.sql
@@ -1,75 +1,72 @@
 -- +goose Up
-
 -- Enable UUID extension
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 -- Users table
-CREATE TABLE users (
-    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    first_name VARCHAR(50) NOT NULL DEFAULT '',
-    last_name VARCHAR(50) NOT NULL  DEFAULT '',
-    bio TEXT NOT NULL DEFAULT '',
-    phone_number TEXT NOT NULL, 
-    email VARCHAR(255) UNIQUE NOT NULL,
-    password_hash TEXT NOT NULL,
-    role TEXT DEFAULT 'member' CHECK (role IN ('member', 'admin')),
-    created_at TIMESTAMP DEFAULT NOW(),
-    updated_at TIMESTAMP DEFAULT NOW(),
-    token_version INT NOT NULL DEFAULT 1
-
-);
+CREATE TABLE
+    users (
+        id UUID PRIMARY KEY DEFAULT uuid_generate_v4 (),
+        first_name VARCHAR(50) NOT NULL DEFAULT '',
+        last_name VARCHAR(50) NOT NULL DEFAULT '',
+        bio TEXT NOT NULL DEFAULT '',
+        phone_number TEXT NOT NULL,
+        email VARCHAR(255) UNIQUE NOT NULL,
+        password_hash TEXT NOT NULL,
+        role TEXT DEFAULT 'member' CHECK (role IN ('member', 'admin')),
+        created_at TIMESTAMP DEFAULT NOW (),
+        updated_at TIMESTAMP DEFAULT NOW (),
+        token_version INT NOT NULL DEFAULT 1,
+        is_banned BOOLEAN DEFAULT FALSE,
+        ban_reason TEXT,
+        ban_until TIMESTAMP,
+        is_permanent_ban BOOLEAN DEFAULT FALSE
+    );
 
 -- Books table
-CREATE TABLE books (
-    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    title VARCHAR(255) NOT NULL,
-    author VARCHAR(100) NOT NULL,
-    description TEXT NOT NULL DEFAULT '',
-    genre TEXT NOT NULL DEFAULT '',
-    published_year INT,
-    isbn TEXT UNIQUE,
-    available_copies INT DEFAULT 1,
-    total_copies INT NOT NULL,
-    created_at TIMESTAMP DEFAULT NOW(),
-    updated_at TIMESTAMP DEFAULT NOW(),
-    image_url TEXT NOT NULL DEFAULT ''
-);
+CREATE TABLE
+    books (
+        id UUID PRIMARY KEY DEFAULT uuid_generate_v4 (),
+        title VARCHAR(255) NOT NULL,
+        author VARCHAR(100) NOT NULL,
+        description TEXT NOT NULL DEFAULT '',
+        genre TEXT NOT NULL DEFAULT '',
+        published_year INT,
+        isbn TEXT UNIQUE,
+        available_copies INT DEFAULT 1,
+        total_copies INT NOT NULL,
+        created_at TIMESTAMP DEFAULT NOW (),
+        updated_at TIMESTAMP DEFAULT NOW (),
+        image_url TEXT NOT NULL DEFAULT ''
+    );
 
 -- Borrows table
-CREATE TABLE borrows (
-    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-    book_id UUID NOT NULL REFERENCES books(id) ON DELETE CASCADE,
-    borrowed_at TIMESTAMP DEFAULT NOW(),
-    due_date TIMESTAMP,
-    returned_at TIMESTAMP
-);
+CREATE TABLE
+    borrows (
+        id UUID PRIMARY KEY DEFAULT uuid_generate_v4 (),
+        user_id UUID NOT NULL REFERENCES users (id) ON DELETE CASCADE,
+        book_id UUID NOT NULL REFERENCES books (id) ON DELETE CASCADE,
+        borrowed_at TIMESTAMP DEFAULT NOW (),
+        due_date TIMESTAMP,
+        returned_at TIMESTAMP
+    );
 
 -- Reviews table
-CREATE TABLE reviews (
-    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-    book_id UUID NOT NULL REFERENCES books(id) ON DELETE CASCADE,
-    rating INT CHECK (rating BETWEEN 1 AND 5),
-    comment TEXT,
-    created_at TIMESTAMP DEFAULT NOW(),
-    updated_at TIMESTAMP DEFAULT NOW()
-
-);
-
--- Sessions table
-CREATE TABLE sessions (
-    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-    refresh_token TEXT NOT NULL,
-    expires_at TIMESTAMP NOT NULL,
-    created_at TIMESTAMP DEFAULT NOW()
-);
+CREATE TABLE
+    reviews (
+        id UUID PRIMARY KEY DEFAULT uuid_generate_v4 (),
+        user_id UUID NOT NULL REFERENCES users (id) ON DELETE CASCADE,
+        book_id UUID NOT NULL REFERENCES books (id) ON DELETE CASCADE,
+        rating INT CHECK (rating BETWEEN 1 AND 5),
+        comment TEXT,
+        created_at TIMESTAMP DEFAULT NOW (),
+        updated_at TIMESTAMP DEFAULT NOW ()
+    );
 
 -- +goose Down
-
-DROP TABLE sessions;
 DROP TABLE reviews;
+
 DROP TABLE borrows;
+
 DROP TABLE books;
+
 DROP TABLE users;
