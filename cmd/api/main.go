68e9208c7c47760f85667c38de650f4e488a0595
Learning React <himelsd117@gmail.com>
2025-11-11 01:19:54 +0600

    --allow-empty-message

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -100,7 +100,7 @@ func main() {
 		reservationGroup.GET("/book/:id", handlers.GetReservationsByBookIDHandler)
 		reservationGroup.GET("/book/:id/user", handlers.GetReservationsByBookIDAndUserIDHandler)
 		reservationGroup.GET("/reservation/:id", handlers.GetReservationsByReservationID)
-		reservationGroup.GET("/", middleware.AdminOnly(), handlers.GetReservationsHandler)
+		reservationGroup.GET("/", handlers.GetReservationsHandler)
 		reservationGroup.PATCH("/:id/status", middleware.AdminOnly(), handlers.UpdateReservationStatusHandler)
 		reservationGroup.GET("/next/:id", middleware.AdminOnly(), handlers.GetNextReservationHandler)
 	}
